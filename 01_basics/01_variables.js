c0b1fc13dd7b2cccf819c5fc675d3ae73e4990ac
Amresh Nirala <138843165+ammunirala@users.noreply.github.com>
2025-12-22 19:06:29 +0000

    varibles

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 552
+let accountEmail = "kjajbck@gmail.com"
+var accountPassword = "54154"
+accountCity = "Bihar"
+let accountState;
+
+accountEmail = "skjdsvds.com"
+accountPassword ="445142"
+accountCity = "up"
+accountState; // undefined
+// accountId = 241512 not allowed because cannot change in const
+
+
+console.table([accountEmail,accountPassword, accountCity, accountState]);
+
+// Notes : alwys prefer let 
