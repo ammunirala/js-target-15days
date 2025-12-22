96a31deebf77738dfc6ddc0fd7c165f3560624d7
Amresh Nirala <138843165+ammunirala@users.noreply.github.com>
2025-12-22 19:28:33 +0000

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+console.log(3+3)
+
+
+console.log("Ammu")
+let name = "ammu"
+let age = 23
+let isLoggesIn = false
+let state;
+
+
+// primitive data types 
+// number => 2 to power 53
+//bigint 
+//string => ""
+//boolean=> true/false
+//null => standalone value
+//undefined =>
+// symboll => unique
+
+
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
