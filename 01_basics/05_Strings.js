8f610b1dc84b5274c289f42e09c102eede96deeb
Amresh Nirala <138843165+ammunirala@users.noreply.github.com>
2025-12-24 15:14:10 +0000

    string

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,5 @@
+const name = "Amresh"
+const repoCount  = 50
+// console.log(name + repoCount );
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // Moodern way of syntax concatination
