3a5f418f906fc8f091f11ea19585da8b2d4fa324
Amresh Nirala <138843165+ammunirala@users.noreply.github.com>
2025-12-24 16:05:20 +0000

    math & num

---
+++ 01_basics/num_and_math.js
@@ -0,0 +1,46 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+// +++++++++++++Maths+++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.6));
+// console.log(Math.min(5,55,55,58,55,9656));
+// console.log(Math.max(1515,61514,51414));
+
+
+console.log(Math.random()); // 0-1
+console.log((Math.random()*10) + 1); //1-10
+console.log(Math.floor(Math.random()*10) + 1); // round value btw 1-10
+
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) // round value btw 10 - 20
+
+
+
+
+
+
+
+
