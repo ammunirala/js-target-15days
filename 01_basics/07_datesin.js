5e9c2af8682398a4a94f4efe7e9dacd810f43427
Amresh Nirala <138843165+ammunirala@users.noreply.github.com>
2025-12-24 16:22:26 +0000

    date

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,24 @@
+// Dates
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleTimeString());
+
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2000,5,22)
+console.log(myCreatedDate.toDateString());
+
+
+
+
+
+
+
+
+
+
